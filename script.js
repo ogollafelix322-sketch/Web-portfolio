32aec083e67234535783a485e80e56bfc78edcd9
Felix Ogolla <Ogollafelix322@gmail.com>
2026-03-11 23:57:13 -0700

    Update script.js
    add console and alert changrs

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 console.log("Javascript connected successfully");
 
 alert("Welcome to Felix Web Development Page");
+
+let country="Kenya";
+alert(country);
+
+let message="Learning Javascript";
+console.log(message);
+
+let age=30;
+console.log(age);
+
+let isDeveloper=true;
+console.log(isDeveloper);
+
+let name="Felix";
+console.log(name);
+
+let num1=20;
+let num2=10;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+
+let apples=3;
+let oranges=5;
+let totalfruit=apples/oranges;
+alert(totalfruit);
+
+let score=75;
+console.log(score>50);
+console.log(score<50);
+console.log(score==75);
+console.log(score!=50);
+
+let height=20;
+alert(height>40);
