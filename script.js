aa8c98c6914f7aa908c786b9fafc8e1bcd80b03a
Felix Ogolla <Ogollafelix322@gmail.com>
2026-03-14 00:16:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,68 @@ console.log(score!=50);
 
 let height=20;
 alert(height>40);
+
+let temperature=30;
+if(temperature>25){
+    alert("It is a hot day");
+}
+
+let number=10;
+if(number<20){
+    console.log("The number is less than 20");
+}
+let pen=30;
+if(number>50){
+    console.log("You Failed");
+}
+
+let marks=60;
+if(marks>=70){
+    alert("Grade A");
+}
+else if(marks>=50){
+    alert("Grade B")
+}
+else{
+    alert("Fail");
+}
+
+let ice=15;
+if(ice>30){
+    console.log("Hot");
+}
+else if(ice>20){
+    console.log("Warm");
+}
+else{
+    console.log("Cold");
+}
+
+function showMessage(){
+    alert("Javascript is a fun");
+}
+showMessage()
+
+function checkNumber(){
+    let number=10;
+    if (number>15){
+        console.log("Number is greater than 15");
+    }
+    else if(number>12){
+        console.log("Number is less than 12");
+    }
+    else{
+        console.log("Number is neither greater than or less than 10");
+    }
+}
+checkNumber()
+
+function showAge(age){
+    alert("You age is " +age);
+}
+showAge(30);
+
+function multiply(a,b){
+    console.log(a*b);
+}
+multiply(4,6);
